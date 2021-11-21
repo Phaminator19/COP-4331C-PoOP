fe2a69eec7da4424231865f2cdd2258b9effc397
Estefania Sanchez <este.sanchez17@gmail.com>
2021-11-21 03:02:59 -0500

    added signup

--- Project_1/app/src/main/java/com/example/project_1/Signup.java
+++ Project_1/app/src/main/java/com/example/project_1/Signup.java
@@ -1,3 +1,25 @@
-public class Signup extends AppCompatActivity {
+package com.example.project_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class Signup extends AppCompatActivity{
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        // this populates the drop down menu with the three user types - Estefania
+        Spinner userType = (Spinner) findViewById(R.id.userType);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.user_types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        userType.setAdapter(adapter);
+
+    }
 
 }
