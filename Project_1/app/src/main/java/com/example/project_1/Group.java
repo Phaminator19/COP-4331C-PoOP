08872548419309b3dd9475021c58322c124cf312
CourtneyThan <courtthan@gmail.com>
2021-11-22 18:23:42 -0500

    Changes

--- Project_1/app/src/main/java/com/example/project_1/Group.java
+++ Project_1/app/src/main/java/com/example/project_1/Group.java
@@ -2,14 +2,39 @@ package com.example.project_1;
 
 public class Group {
     private String GroupName;
-    private String GroupPhoto;
-    private String GroupVideo;
+    private String GroupDescription;
+    private Integer NumOfMembers;
+    private String[] MemberList;
 
     public void setGroupName (String GroupName) {
-         this.GroupName = GroupName;
+        this.GroupName = GroupName;
     }
 
     public String getGroupName () {
         return GroupName;
+    }
+
+    public void setGroupDescription (String GroupDescription) {
+        this.GroupDescription = GroupDescription;
+    }
+
+    public String setGroupDescription() {
+        return GroupDescription;
+    }
+
+    public void setNumOfMembers (int NumOfMembers) {
+        this.NumOfMembers = NumOfMembers;
+    }
+
+    public Integer getNumOfMembers () {
+        return NumOfMembers;
+    }
+
+    public void setMemberList (String[] MemberList) {
+        this.MemberList = MemberList;
+    }
+
+    public String[] MemberList () {
+        return MemberList;
     }
 }
