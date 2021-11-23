aad9f9388fd50060530c5cd6876026dbe1ffe567
Quang <70238159+Phaminator19@users.noreply.github.com>
2021-11-22 19:55:32 -0500

    This is bad folder and doesn't work

--- Project_1/app/src/main/java/com/example/project_1/ListType.java
+++
@@ -1,7 +0,0 @@
-package com.example.project_1;
-
-public abstract class ListType {
-    public abstract void addFriend(User user);
-    public abstract void removeFriend(int index);
-    public abstract void view();
-}
--- Project_1/app/src/main/java/com/example/project_1/UserActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.project_1;
-
-public class UserActivity {
-    private String UserName;
-    public AccountSetting accountSetting;
-
-}
