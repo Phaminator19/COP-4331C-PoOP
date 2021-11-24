c36fee78593eeb5843b9fba80c63601dd353eb83
Estefania Sanchez <este.sanchez17@gmail.com>
2021-11-23 21:55:26 -0500

    update java files

--- Project_1/app/src/main/java/com/example/project_1/Login.java
+++ Project_1/app/src/main/java/com/example/project_1/Login.java
@@ -1,7 +1,35 @@
 package com.example.project_1;
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Login extends AppCompatActivity {
+    Button loginButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+
+        loginButton = (Button) findViewById(R.id.login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(Login.this));
+            }
+        });
+
+    }
 
 }
