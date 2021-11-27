988e4984e735f96ba07370344196ef031001332c
Estefania Sanchez <este.sanchez17@gmail.com>
2021-11-27 02:20:24 -0500

    Update Message.java

--- Project_1/app/src/main/java/com/example/project_1/Message.java
+++ Project_1/app/src/main/java/com/example/project_1/Message.java
@@ -2,15 +2,13 @@ package com.example.project_1;
 
 public class Message {
 
-    private String message; // message body
-    private String userId; // data of the user that sent this message
+    private String message; 
+    private String userId;
     private String userName;
     private String timestamp;
-    //@Exclude
-    //private boolean belongsToCurrentUser;
 
     public Message() {
-        this.message = "kalispera";
+        this.message = "empty";
     }
 
     public Message(String message) {
